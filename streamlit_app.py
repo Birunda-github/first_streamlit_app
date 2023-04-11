d21ce764f1c8e5b1f3ab3ae8dc793222a01e676e
Birunda-github <130438329+Birunda-github@users.noreply.github.com>
2023-04-11 13:26:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit as st
 st.title('My Parents new diner')
 st.header('Breakfast Menu')
 
-st.subheader('Oatmeal')
-st.subheader('Spinach Smoothie')
-st.subheader('Boiled Egg')
+st.text('Oatmeal')
+st.text('Spinach Smoothie')
+st.text('Boiled Egg')
