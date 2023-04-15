8a37c31b4c50525f93d8c58b68dbc72b010796d4
Birunda-github <130438329+Birunda-github@users.noreply.github.com>
2023-04-16 00:13:47 +0530

    add fruityvice header

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,8 @@ fruits_to_show=my_fruit_list.loc[fruit_selected]
 #st.dataframe(my_fruit_list)
 st.dataframe(fruits_to_show)
 
+st.header('Fruityvice fruit advice!')
+
 fruityvice_response=requests.get('https://fruityvice.com/api/fruit/watermelon')
 st.text(fruityvice_response)
 
