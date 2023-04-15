09fd63fbdb94300ceb88ab9d08c349a1ffa2571d
Birunda-github <130438329+Birunda-github@users.noreply.github.com>
2023-04-16 00:18:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ fruityvice_response=requests.get('https://fruityvice.com/api/fruit/banana')
 
 st.text(fruityvice_response.json())
 
-fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized=p.json_normalize(fruityvice_response.json())
 
 st.dataframe(fruityvice_normalize)
 
