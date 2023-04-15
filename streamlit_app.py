ca7c148706d47b8860d7eec63438fdf16ffa1e9a
Birunda-github <130438329+Birunda-github@users.noreply.github.com>
2023-04-16 00:42:22 +0530

    getting input

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,9 @@ st.dataframe(fruits_to_show)
 
 st.header('Fruityvice fruit advice!')
 
+fruit_choice=st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered',fruit_choice)
+
 fruityvice_response=requests.get('https://fruityvice.com/api/fruit/'+'Kiwi')
 
 st.text(fruityvice_response.json())
