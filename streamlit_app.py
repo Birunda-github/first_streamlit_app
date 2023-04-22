874bfa27bb20a1c9e59f326f65949ef23b598c1f
Birunda-github <130438329+Birunda-github@users.noreply.github.com>
2023-04-23 00:44:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,9 +65,19 @@ if st.button('Get Fruit Load list'):
   my_data_rows=get_fruit_load_list()
   st.dataframe(my_data_rows)
 st.stop()
+
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
+    return "Thanks for adding "+new_fruit
+  
 add_my_fruit=st.text_input('What would you like to choose?','Mango')
-st.write('Thanks for adding ',add_my_fruit)
+if st.button('Add a fruit to the list'):
+  my_cnx=snowflake.connector.connect(**st.secrets["snowflake"])
+  back_from_function=insert_row_snowflake(add_my_fruit)
+  st.text(back_from_function)
 
 
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
+
+
 
