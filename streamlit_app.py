2d81b7d9e4f5f480f499b44dc56d8e170654bece
Birunda-github <130438329+Birunda-github@users.noreply.github.com>
2023-04-22 23:30:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,13 +44,14 @@ my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
-#st.text("Hello from Snowflake:")
-st.header("The Fruit List contains:")
 
-#return one value
-#st.text(my_data_row)  
+#returns only one value
+#my_data_row = my_cur.fetchone()
 
 #returns multiple value
-st.dataframe(my_data_row)
+my_data_rows=my_cur.fetchall()
+#st.text("Hello from Snowflake:")
+st.header("The Fruit List contains:")
+#st.text(my_data_row)  
+st.dataframe(my_data_rows)
 
